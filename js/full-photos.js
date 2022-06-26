4c193da3091043f90c83c02408ff3cf7993acaaa
Aleksey Boyarchenko <asboyarchenko@gmail.com>
2022-06-26 21:29:51 +0300

    Добавлен сценарий просмотра фотографий в полноразмерном режиме

---
+++ js/full-photos.js
@@ -0,0 +1,61 @@
+function createRandomFullPhoto (photos) {
+
+  const body = document.querySelector('body');
+  const previewPhoto = document.querySelectorAll('.picture');
+  const fullPhoto = document.querySelector('.big-picture');
+  const photoImage = fullPhoto.querySelector('.big-picture__img').querySelector('img');
+  const likesCount = fullPhoto.querySelector('.likes-count');
+  const commentsList = fullPhoto.querySelector('.social__comments');
+  const commentItem = commentsList.querySelector('.social__comment');
+  const photoDescription = fullPhoto.querySelector('.social__caption');
+  const commentCounter = fullPhoto.querySelector('.social__comment-count');
+  const commentLoader = fullPhoto.querySelector('.comments-loader');
+  const closeButton = fullPhoto.querySelector('.big-picture__cancel');
+
+  const commentListFragment = document.createDocumentFragment();
+
+  function addPhotoClickHandler (photo, {url, likes, description, comments}) {
+    photo.addEventListener('click', () => {
+      fullPhoto.classList.remove('hidden');
+      commentCounter.classList.add('hidden');
+      commentLoader.classList.add('hidden');
+      body.classList.add('modal-open');
+
+      photoImage.src = url;
+      likesCount.textContent = likes;
+      photoDescription.textContent = description;
+
+      commentsList.textContent = '';
+
+      comments.forEach((comment) => {
+        const commentElementCopy = commentItem.cloneNode(true);
+        const commentAvatar = commentElementCopy.querySelector('.social__comment .social__picture');
+        const commentMesssage = commentElementCopy.querySelector('.social__comment .social__text');
+        commentAvatar.src = comment.avatar;
+        commentAvatar.alt = comment.name;
+        commentMesssage.textContent = comment.message;
+        commentListFragment.append(commentElementCopy);
+      });
+
+      commentsList.append(commentListFragment);
+    });
+  }
+
+  for (let i = 0; i < previewPhoto.length; i++) {
+    addPhotoClickHandler(previewPhoto[i], photos[i]);
+  }
+
+  closeButton.addEventListener ('click', () => {
+    fullPhoto.classList.add('hidden');
+    body.classList.remove('modal-open');
+  });
+
+  document.addEventListener('keydown', (evt) => {
+    if (evt.key === 'Escape') {
+      fullPhoto.classList.add('hidden');
+      body.classList.remove('modal-open');
+    }
+  });
+}
+
+export { createRandomFullPhoto };
--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 import {createPhotosDecriptions} from './data.js';
 import {createRandomPhoto} from './preview-photos.js';
+import {createRandomFullPhoto} from './full-photos.js';
 
 const PHOTO = createPhotosDecriptions();
+
 createRandomPhoto(PHOTO);
+createRandomFullPhoto(PHOTO);
